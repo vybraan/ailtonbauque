c38011474fa0c4ef0ad8a76b3c49bdd83f7b740e
Ailton Baúque <ailton.bauque@gmail.com>
2022-12-27 23:55:33 +0200

    UPTATE: dark mode

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
 import Header from './components/Header';
+import banner from './banner.png';
 
 const Home = () =>{
 	return(
 		<>
 		<Header />
+		<div className="bg-gray-800 text-white p-2 text-center">
+			<div>
+				<span>I am a Bravian Programmer</span>
+				<h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Software</span> Developer.</h1>
+				<p>a very beatiful text to supper what I am, my motivation</p>
+				<button className="bg-purple-600 text-blue-100 rounded-md font-bold px-3 py-2 mt-3">Get in touch</button>
+			</div>
+		{//<img alt="Ailton's " src={banner}/>
+		}
+		</div>
 		</>
 	);
 }
